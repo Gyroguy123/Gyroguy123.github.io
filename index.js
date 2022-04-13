d473d0d6e76476f2bd7bc904b487b17c3a98e8b1
Gyroguy123 <pjacks22@my.centennialcollege.ca>
2022-04-13 03:21:06 -0400

    Another try

---
+++ index.js
@@ -0,0 +1,39 @@
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+var imgIndex = 1;
+var timer = null;
+showImg(imgIndex);
+
+function changeImage(n) {
+  clearTimeout(timer);
+  showImg(imgIndex += n);
+}
+
+function currentSlide(n) {
+  clearTimeout(timer);
+  showImg(imgIndex = n);
+}
+
+function showImg(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n==undefined){n = ++imgIndex}
+  if (n > slides.length) {imgIndex = 1}
+  if (n < 1) {imgIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[imgIndex-1].style.display = "block";
+
+//   Image changes automatically in 5 seconds
+  timer = setTimeout(showImg, 5000);
+}
